c5d6f1dca77ce21ca07b6dd3aeebeb6101f4097d
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-08-29 16:33:40 +0200

    Setup btbd-packets directory
    BTBD #29

---
+++ ts_sources/sources/utility/btbd-packets/Packet.test.ts
@@ -0,0 +1,13 @@
+declare var describe;
+declare var expect;
+declare var test;
+
+import { Packet } from './Packet';
+
+describe('Packet Test Suite', () => {
+
+    test('placeholder', () => {
+        const pack = new Packet('hey');
+    });
+
+});
