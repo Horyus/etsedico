c244ecc794cef3d85ae967f063f8ef4483cba7b7
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-11-01 18:04:34 -0400

    CryptoPlugin Implementation
    BTBD #20

--- ts_sources/sources/Battlebird.ts
+++ ts_sources/sources/Battlebird.ts
@@ -1,6 +1,8 @@
 import { Bush }            from './Bush';
 import { TimestampPlugin } from './core/timestamp';
 import { EventEmitter }    from 'events';
+import { ECKeyPair }       from './utility/btbd-crypto/ec_gen';
+import { CryptoPlugin }    from './core/crypto';
 
 export class Battlebird {
 
@@ -8,12 +10,18 @@ export class Battlebird {
     private readonly event: EventEmitter = new EventEmitter();
     private readonly env: any = {};
 
-    constructor() {
+    private readonly eckeypair: ECKeyPair;
+    private readonly master_signature: Buffer;
+
+    constructor(eck: ECKeyPair, master_signature: Buffer) {
         this.bush = new Bush({event: this.event, env: this.env});
+        this.eckeypair = eck;
+        this.master_signature = master_signature;
     }
 
     public async start(): Promise<void> {
         this.bush.plug(new TimestampPlugin((): number => Date.now()));
+        this.bush.plug(new CryptoPlugin(this.eckeypair, this.master_signature));
 
         await this.bush.start();
     }
